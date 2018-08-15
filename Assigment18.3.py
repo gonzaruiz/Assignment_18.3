f4811ebb02046b1aaaf4527f984bb4f00e06cdf9
gonzalo ruiz <38412697+gonzaruiz@users.noreply.github.com>
2018-08-15 12:34:16 +0800

    Add files via upload

---
+++ Assigment18.3.py
@@ -0,0 +1,17 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+Z = X - M(mu)/SD 
+Z = (1100-1026)/209
+Z = 0.35
+
+p-value correponding to z-score 0.35 = 0.1368
+
+p-value = 0.5000 + 0.1368 = 0.6368 (63.68 %)
+
+Results :
+I did the SAT test better than the 63.38% of the other students
+
